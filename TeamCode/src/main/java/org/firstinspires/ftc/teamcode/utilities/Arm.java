c250566ecee2bf2de5c66ef0880fe43e2c669d97
mudasir <96320211+mud-ali@users.noreply.github.com>
2024-10-18 16:57:00 -0400

    clean up arm class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utilities/Arm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utilities/Arm.java
@@ -7,43 +7,37 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class Arm {
     DcMotor armMotor;
+    public boolean slowMode = false;
+    
+    // encoder ticks per 360 degrees for 312 rpm motor
+    public static double ENCODER_TICKS = 537.7;
 
-    public static double ENCODER_TICKS = 1425*3;
 
+    // the below is archaic and very mostly likely wrong pls ignore it
     /* ENCODER TICKS EXPLAINED:
-    Originally, there are 103.8 encoder ticks, which means that for one 360 degree rotation, the encoder ticks 103.8 times (for 1150 rpm motor)
     28 rotations of worm gear : 1 rotation of the gear
     since the motor directly turns the worm gear, that means that now, the gear that is connected to the lift will turn slower
     in other words, it will turn 28 times slower
     that means in one motor rotation, there will be 537.6/28 encoder ticks */
 
-    // angle of vertical arm from robot arm's minimum position
-    public static double VERTICAL_ANGLE = 122;
+    // position presets
+    private static final double INIT_ANGLE = 0;
+    private static final double DROP_ANGLE = 30;
+    private static final double PICKUP_ANGLE = 210;
 
-    public static double MAX_LIMIT = VERTICAL_ANGLE + 30; // upper limit
-    public static double MIN_LIMIT = -5; // lower limit for manual lift
-
-    // higher power lift going up vs going down cause power + gravity = more power
-    public static double FULL_POWER = 0.75;
-    public static double SLOW_POWER = 0.40;
-
+    public static double FULL_POWER = 1;
+    public static double SLOW_POWER = 0.8 * FULL_POWER;
 
     public Arm(HardwareMap hmap) {
         this.armMotor = hmap.dcMotor.get(CONFIG.armMotor);
-        setInit();
-    }
-
-    public void setInit() {
-         // makes it so the motor is not loose
         armMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         armMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         armMotor.setZeroPowerBehavior(BRAKE);
     }
 
-    //completely stop lift
+    // completely stop lift
     public void stop() {
         armMotor.setPower(0);
-        armMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
     }
 
     // purpose: make sure you don't interrupt a preset
@@ -54,73 +48,38 @@ public class Arm {
     // overload the other setPower to default slowMode to false
 
     /**
-     * @param dir: false = to pick up, true = to drop
+     * @param dir: false = to back, true = towards init
      */
     public void setPower(boolean dir) {
-        if (dir) {
-            if (getPosition() < VERTICAL_ANGLE - 45) {
-                armMotor.setPower(FULL_POWER);
-            }
-            else if (getPosition() >= VERTICAL_ANGLE - 45 && getPosition() < (MAX_LIMIT)) {
-                armMotor.setPower(SLOW_POWER);
-            }
-            else {
-                armMotor.setPower(0);
-            }
-
-        }
-        else {
-
-            if (getPosition() > VERTICAL_ANGLE - 40) {
-                armMotor.setPower(-SLOW_POWER); //TODO: JUNA, DO THIS
-            }
-            else {
-                armMotor.setPower(-SLOW_POWER);
-            }
-        }
+        armMotor.setPower(FULL_POWER * (dir ? 1 : -1));
     }
 
-    // Returns lift position in degrees, robot centric (minimum is 0)
-    public double getPosition(){
-        // Initially gets back position in terms of encoder ticks, which converts to degrees
-        // return (360* ((double) armMotor.getCurrentPosition())/ENCODER_TICKS);
+    // Returns lift position in degrees, robot centric (init pos is 0)
+    public double getPosition() {
         return -armMotor.getCurrentPosition() / ENCODER_TICKS * 360;
-
     }
 
     public boolean runToPosition(double angle) {
-        // takes the angle we want it to go to and makes sure the angle is within range
-        // just in case wrong input
         // converts angle into encoder ticks and then runs to position
+        // with input validation
         armMotor.setTargetPosition((int) (ENCODER_TICKS *-angle/360));
         armMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         // with run to position always positive power (setPower will be the one determining direction)
         // run to position is always in presets or else it'll be jittery
         armMotor.setPower(-0.3);
 
-        return true; // to confirm that it runs
+        return true;
     }
 
-    public boolean toPickUp(){ // preset for picking up pixels
-        return runToPosition(0);
-    }
-
-    public boolean toDrop(){ // preset for dropping pixels
-        final double x = 135;
-
-        return runToPosition(x);
-    }
-
-    public boolean drivingPos() {
-        return runToPosition(10);
-    }
-
-    public void toBackBoard(){ // manual drop
+    /**
+     * Directions for manual control
+     */
+    public void setDirectionTowardsInit() {
         armMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         setPower(false);
     }
 
-    public void toRobot(){ // manual pick up
+    public void setDirectionAwayFromInit() {
         armMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         setPower(true);
     }
