18db95ca46401890aaead80e5ab3b329f8ba4e21
Connor <114190604+batmanual9595@users.noreply.github.com>
2024-10-19 22:05:59 -0400

    Teleop Stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/Teleop.java
@@ -39,9 +39,9 @@ public class Teleop extends OpMode {
             telemetry.addData("open", claw.getPosition());
         } else if (gamepad1.right_bumper || gamepad2.right_bumper) {
             claw.close();
-            telemetry.addData("close", claw.getPosition());
+            telemetry.addData("close" +
+                    "++, claw.getPosition());
         }
-
         // Slides controls
         if (gamepad1.y || gamepad2.y) {
             slides.high();
