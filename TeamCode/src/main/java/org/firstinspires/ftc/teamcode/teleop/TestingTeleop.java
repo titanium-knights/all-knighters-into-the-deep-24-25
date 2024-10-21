a67b1838c0723f5c3e7fa58b87586afa5bb3d8a6
mudasir <96320211+mud-ali@users.noreply.github.com>
2024-10-21 19:41:02 -0400

    arm reports position

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/TestingTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/TestingTeleop.java
@@ -10,40 +10,18 @@ import org.firstinspires.ftc.teamcode.utilities.Slides;
 
 @TeleOp(name="testing Teleop", group="default")
 public class TestingTeleop extends OpMode {
-
-    private SimpleMecanumDrive drive;
-    private Claw claw;
     private Slides slides;
     private Arm arm;
 
-    final float STICK_MARGIN = 0.5f;
-    final double normalPower = 0.85;
-
     @Override
     public void init() {
-        // initialize util classes for hardware
-        drive = new SimpleMecanumDrive(hardwareMap);
-        claw = new Claw(hardwareMap);
         slides = new Slides(hardwareMap);
         arm = new Arm(hardwareMap);
     }
 
     @Override
     public void loop() {
-        if (time < 2.23) {
-            telemetry.addData("arm to init", arm.getPosition());
-            arm.setDirectionTowardsInit();
-            claw.goToFoldedPosition();
-        }
-        if (time > 2.35 && time < 4.78) {
-            telemetry.addData("arm to pickup", arm.getPosition());
-            arm.setDirectionAwayFromInit();
-            claw.goToPickUpPosition();
-        }
-        if (time > 4.78 && time < 6.23) {
-            telemetry.addData("stop", arm.getPosition());
-            arm.stop();
-        }
+        telemetry.addData("arm position", arm.getPosition());
         telemetry.update();
     }
 }
