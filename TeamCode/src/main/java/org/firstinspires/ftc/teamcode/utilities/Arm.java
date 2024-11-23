9b0ce76825b381b0cbccca8fce32c434d7b2092c
mudasir <96320211+mud-ali@users.noreply.github.com>
2024-11-23 15:54:12 -0500

    add specimen pickup position to arm; tune values

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utilities/Arm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utilities/Arm.java
@@ -2,12 +2,14 @@ package org.firstinspires.ftc.teamcode.utilities;
 
 import androidx.annotation.NonNull;
 
+import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
 import com.acmerobotics.roadrunner.Action;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 
 
+@Config
 public class Arm {
     Servo arm;
     public Arm(HardwareMap hmap) {
@@ -16,11 +18,34 @@ public class Arm {
     public void beforePickUp() {
         arm.setPosition(0.60);
     }
-    public void pickingUp() {arm.setPosition(0.68);}
-    public void toInitPos() { arm.setPosition(0.0); }
-    public void toScoreBucketPos() { arm.setPosition(0.25); }
-    public void toScoreSpecimenPos() { arm.setPosition(0.25); }
-    public double getPosition() { return arm.getPosition();}
+
+    public static double specimenPickupPosition = 0.45;
+    public static double specimenScorePosition = 0.3;
+
+    public void pickingUpSpecimen() {
+        arm.setPosition(specimenPickupPosition);
+    }
+
+    public void pickingUp() {
+        arm.setPosition(0.68);
+    }
+
+    public void toInitPos() {
+        arm.setPosition(0.01);
+    }
+
+    public void toScoreBucketPos() {
+        arm.setPosition(0.25);
+    }
+
+    public void toScoreSpecimenPos() {
+        arm.setPosition(specimenScorePosition);
+    }
+
+    public double getPosition() {
+        return arm.getPosition();
+    }
+
     public class toScoreSpecimenPosAction implements Action {
         @Override
         public boolean run(@NonNull TelemetryPacket packet) {
@@ -37,7 +62,11 @@ public class Arm {
         }
     }
 
-    public Action toInitPosAction() { return new toInitPosAction(); }
+    public Action toInitPosAction() {
+        return new toInitPosAction();
+    }
 
-    public Action toScoreSpecimenPosAction() { return new toScoreSpecimenPosAction(); }
+    public Action toScoreSpecimenPosAction() {
+        return new toScoreSpecimenPosAction();
+    }
 }
