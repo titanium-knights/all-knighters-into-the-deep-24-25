965d6eba0fb73290000407168eb350693a4b11c3
mudasir <96320211+mud-ali@users.noreply.github.com>
2024-12-16 17:28:25 -0500

    add mecanum drive and config

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utilities/CONFIG.java
@@ -0,0 +1,32 @@
+package org.firstinspires.ftc.teamcode.utilities;
+
+import com.acmerobotics.dashboard.config.Config;
+
+@Config
+public class CONFIG {
+    //TODO: label all strings with associated ports and types.
+    // ex: ch motor 3 (control hub > motor > port 3)
+
+    // drivetrain wheels
+    public static String FRONT_LEFT = "fl";  // ch motor 0
+    public static String FRONT_RIGHT = "fr"; // ch motor 2
+    public static String BACK_LEFT = "bl";   // ch motor 3
+    public static String BACK_RIGHT = "br";  // ch motor 1
+
+    // webcam
+    public static String webcam = "Webcam 1";
+
+    // intake
+    public static String clawServoBottom = "clawB";
+    public static String clawRotator = "clawR";
+    public static String scissorsServo = "scissors";
+
+    // outtake:
+    public static String slidesMotor = "slidesMotor";
+    public static String armServo = "armServo";
+    public static String clawServo = "claw";
+
+    // yuma's active intake
+    public static String rightIntake = "rightIntake";
+    public static String leftIntake = "leftIntake";
+}
