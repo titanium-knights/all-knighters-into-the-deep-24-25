cbd49d2cc0e515f3a071cd95bed577e578812120
iDrg899 <omnom899@gmail.com>
2024-12-19 16:40:48 -0500

    initial commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utilities/Arm.java
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode.utilities;
+
+public class Arm {
+    private Servo armServo;
+
+    private final double INIT_POSITION = 0.3;
+    private final double RECEIVING_POSITION = 0.1;
+    private final double SCORE_BUCKET_POSITION = 0.65;
+
+
+}
