e4447f09ce12620e76e6fe047760362aa6d98356
mudasir <96320211+mud-ali@users.noreply.github.com>
2025-01-12 18:38:49 -0500

    completed up to 5.2

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/constants/FConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/constants/FConstants.java
@@ -28,9 +28,9 @@ public class FConstants {
 
         FollowerConstants.xMovement = 66.9373113037985;
         FollowerConstants.yMovement = 48.1538552818054;
-
-        FollowerConstants.forwardZeroPowerAcceleration = -41.278;
-        FollowerConstants.lateralZeroPowerAcceleration = -59.7819;
+        
+        FollowerConstants.forwardZeroPowerAcceleration = -50.388670001990285;
+        FollowerConstants.lateralZeroPowerAcceleration = -83.29155624482641;
 
         FollowerConstants.translationalPIDFCoefficients.setCoefficients(0.1,0,0.01,0);
         FollowerConstants.useSecondaryTranslationalPID = false;
