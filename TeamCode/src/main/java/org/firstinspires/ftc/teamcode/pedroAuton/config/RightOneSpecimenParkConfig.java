c94b8d6791d5c7f7aabcce69b4d4835ecd41aa38
lunabree <114190448+lunabree@users.noreply.github.com>
2025-03-09 14:46:17 -0400

    auton tuned for right side

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroAuton/config/RightOneSpecimenParkConfig.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroAuton/config/RightOneSpecimenParkConfig.java
@@ -19,9 +19,9 @@ public class RightOneSpecimenParkConfig implements IAutonConfig {
     public static final Pose START_POSE = new Pose(9, 62, Math.toRadians(0));
     // Pose for scoring the preloaded specimen (drives the robot closer to the bar).
     public static final Pose SCORE_SPECIMEN_BAR_POSE = new Pose(39, 62, Math.toRadians(0));
-    public static final Pose SCORE_SPECIMEN_BAR_POSE_1 = new Pose(39.5, 66, Math.toRadians(0));
-    public static final Pose SCORE_SPECIMEN_BAR_POSE_2 = new Pose(39.5, 70, Math.toRadians(0));
-    public static final Pose SCORE_SPECIMEN_BAR_POSE_3 = new Pose(39.5, 73, Math.toRadians(0));
+    public static final Pose SCORE_SPECIMEN_BAR_POSE_1 = new Pose(36.5, 70, Math.toRadians(2));
+    public static final Pose SCORE_SPECIMEN_BAR_POSE_2 = new Pose(36.5, 76, Math.toRadians(2));
+    public static final Pose SCORE_SPECIMEN_BAR_POSE_3 = new Pose(37, 80, Math.toRadians(2));
 
     // Pose for aligning before specimen retrieval.
     public static final Pose ALIGN_TO_PREPARE_FOR_RETRIEVAL = new Pose(30, 36, Math.toRadians(0));
@@ -33,8 +33,8 @@ public class RightOneSpecimenParkConfig implements IAutonConfig {
 
     // Poses for the second retrieval motion.
     public static final Pose RETRIEVE_SPECIMEN_POSE2_1 = new Pose(60, 27, Math.toRadians(5));
-    public static final Pose RETRIEVE_SPECIMEN_POSE2_2 = new Pose(60, 20, Math.toRadians(5));
-    public static final Pose ENDING_POINT2 = new Pose(20, 20, Math.toRadians(0));
+    public static final Pose RETRIEVE_SPECIMEN_POSE2_2 = new Pose(60, 19, Math.toRadians(5));
+    public static final Pose ENDING_POINT2 = new Pose(20, 19, Math.toRadians(0));
 
     // Poses for the third retrieval motion.
     public static final Pose RETRIEVE_SPECIMEN_POSE3_1 = new Pose(60, 20, Math.toRadians(0));
@@ -42,10 +42,10 @@ public class RightOneSpecimenParkConfig implements IAutonConfig {
     public static final Pose ENDING_POINT3 = new Pose(25, 12, Math.toRadians(0));
 
     // Poses for scoring additional specimen.
-    public static final Pose GET_SPECIMEN_POSE_1 = new Pose(25, 23.9, Math.toRadians(5));
-    public static final Pose GET_SPECIMEN_POSE_2 = new Pose(28.5, 28, Math.toRadians(0));
-    public static final Pose GET_SPECIMEN_POSE_3 = new Pose(28.5, 28, Math.toRadians(0));
-    public static final Pose ALIGN_TO_SCORE_POSE = new Pose(25, 65, Math.toRadians(0));
+    public static final Pose GET_SPECIMEN_POSE_1 = new Pose(24.4, 23.8, Math.toRadians(8));
+    public static final Pose GET_SPECIMEN_POSE_2 = new Pose(26.5, 27.6, Math.toRadians(8));
+    public static final Pose GET_SPECIMEN_POSE_3 = new Pose(26.6, 27.6, Math.toRadians(7));
+    public static final Pose ALIGN_TO_SCORE_POSE = new Pose(25, 65, Math.toRadians(7));
 
     // Parking Pose
     public static final Pose PARK_POSE = new Pose(13, 27, Math.toRadians(0));
@@ -104,7 +104,6 @@ public class RightOneSpecimenParkConfig implements IAutonConfig {
             new AutonStepDescriptor("HORI_SPECOUT_CLOSED"),
             new AutonStepDescriptor((double) GRAB_SPECIMEN_WAIT_MS / 1000.0),
             new AutonStepDescriptor("HORI_TRANSFER_BOTTOMCLOSED_TOPOPEN"),
-            new AutonStepDescriptor((double) GRAB_SPECIMEN_WAIT_MS / 1000.0),
             new AutonStepDescriptor("HORI_TRANSFER_BOTTOMCLOSED_TOPCLOSED"),
             new AutonStepDescriptor(WAIT_PLS),
             new AutonStepDescriptor("HORI_TRANSFER_BOTTOMOPEN_TOPCLOSED"),
@@ -118,7 +117,8 @@ public class RightOneSpecimenParkConfig implements IAutonConfig {
             new AutonStepDescriptor("BOTTOM_OPEN"),
 
             // --- Re-align to get specimen again ---
-            new AutonStepDescriptor(SCORE_SPECIMEN_BAR_POSE_1, GET_SPECIMEN_POSE_2),  // Drive to pickup alignment.
+            new AutonStepDescriptor(SCORE_SPECIMEN_BAR_POSE_1, ALIGN_TO_SCORE_POSE),  // Drive to pickup alignment.
+            new AutonStepDescriptor(ALIGN_TO_SCORE_POSE, GET_SPECIMEN_POSE_2),
             new AutonStepDescriptor((double) SEGMENT_SLEEP_TIME_MS / 1000.0),
 
             // 3RD SPECIMEN
@@ -144,33 +144,35 @@ public class RightOneSpecimenParkConfig implements IAutonConfig {
             new AutonStepDescriptor("BOTTOM_OPEN"),
 
             // --- Re-align to get specimen again ---
-            new AutonStepDescriptor(SCORE_SPECIMEN_BAR_POSE_2, GET_SPECIMEN_POSE_3),  // Drive to pickup alignment.
-            new AutonStepDescriptor((double) SEGMENT_SLEEP_TIME_MS / 1000.0),
+            new AutonStepDescriptor(SCORE_SPECIMEN_BAR_POSE_2, ALIGN_TO_SCORE_POSE),  // Drive to pickup alignment.
+            //new AutonStepDescriptor(ALIGN_TO_SCORE_POSE, GET_SPECIMEN_POSE_3),
+            //new AutonStepDescriptor((double) SEGMENT_SLEEP_TIME_MS / 1000.0),
 
             // 4TH SPECIMEN
             // --- Get the specimen from human player ---
-            new AutonStepDescriptor("HORI_SPECIN_OPEN"),
-            new AutonStepDescriptor(WAIT_PLS),
-            new AutonStepDescriptor("HORI_SPECOUT_OPEN"),
-            new AutonStepDescriptor(WAIT_PLS),
-            new AutonStepDescriptor("HORI_SPECOUT_CLOSED"),
-            new AutonStepDescriptor((double) GRAB_SPECIMEN_WAIT_MS / 1000.0),
-            new AutonStepDescriptor("HORI_TRANSFER_BOTTOMCLOSED_TOPOPEN"),
-            new AutonStepDescriptor((double) GRAB_SPECIMEN_WAIT_MS / 1000.0),
-            new AutonStepDescriptor("HORI_TRANSFER_BOTTOMCLOSED_TOPCLOSED"),
-            new AutonStepDescriptor(WAIT_PLS),
-            new AutonStepDescriptor("HORI_TRANSFER_BOTTOMOPEN_TOPCLOSED"),
-            new AutonStepDescriptor(WAIT_PLS),
-            new AutonStepDescriptor("MEDIUM_CLOSED"),
-
-            // --- Score specimen position ---
-            new AutonStepDescriptor(GET_SPECIMEN_POSE_3, ALIGN_TO_SCORE_POSE), // Move closer to the submersible for scoring
-            new AutonStepDescriptor(ALIGN_TO_SCORE_POSE, SCORE_SPECIMEN_BAR_POSE_3),
-            new AutonStepDescriptor(SCORING_INITIAL_WAIT_SECONDS),              // Wait for initial positioning.
-            new AutonStepDescriptor("BOTTOM_OPEN"),
+//            new AutonStepDescriptor("HORI_SPECIN_OPEN"),
+//            new AutonStepDescriptor(WAIT_PLS),
+//            new AutonStepDescriptor("HORI_SPECOUT_OPEN"),
+//            new AutonStepDescriptor(WAIT_PLS),
+//            new AutonStepDescriptor("HORI_SPECOUT_CLOSED"),
+//            new AutonStepDescriptor((double) GRAB_SPECIMEN_WAIT_MS / 1000.0),
+//            new AutonStepDescriptor("HORI_TRANSFER_BOTTOMCLOSED_TOPOPEN"),
+//            new AutonStepDescriptor((double) GRAB_SPECIMEN_WAIT_MS / 1000.0),
+//            new AutonStepDescriptor("HORI_TRANSFER_BOTTOMCLOSED_TOPCLOSED"),
+//            new AutonStepDescriptor(WAIT_PLS),
+//            new AutonStepDescriptor("HORI_TRANSFER_BOTTOMOPEN_TOPCLOSED"),
+//            new AutonStepDescriptor(WAIT_PLS),
+//            new AutonStepDescriptor("MEDIUM_CLOSED"),
+//
+//            // --- Score specimen position ---
+//            new AutonStepDescriptor(GET_SPECIMEN_POSE_3, ALIGN_TO_SCORE_POSE), // Move closer to the submersible for scoring
+//            new AutonStepDescriptor(ALIGN_TO_SCORE_POSE, SCORE_SPECIMEN_BAR_POSE_3),
+//            new AutonStepDescriptor(SCORING_INITIAL_WAIT_SECONDS),              // Wait for initial positioning.
+//            new AutonStepDescriptor("BOTTOM_OPEN"),
 
             // --- Park ---
-            new AutonStepDescriptor(SCORE_SPECIMEN_BAR_POSE_3, PARK_POSE),  // Drive to pickup alignment.
+            // todo: change to score specimen pose 3 if we get 4th specimen working
+            new AutonStepDescriptor(ALIGN_TO_SCORE_POSE, PARK_POSE),  // Drive to pickup alignment.
             new AutonStepDescriptor((double) SEGMENT_SLEEP_TIME_MS / 1000.0)
     ));
 
