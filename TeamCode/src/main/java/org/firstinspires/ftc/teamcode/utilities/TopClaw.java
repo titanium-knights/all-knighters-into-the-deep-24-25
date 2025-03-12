82c483f49824d9cdc0dc8c3491d1a2e954aa5d7b
Daniel <danielgtravels@gmail.com>
2025-03-12 16:23:50 -0400

    added maintain position method

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utilities/TopClaw.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utilities/TopClaw.java
@@ -34,5 +34,9 @@ public class TopClaw {
     public double getPosition() {
         return clawOpener.getPosition() / TopClaw.servoAngleModifier;
     }
+
+    public void keep_position(){
+        clawOpener.setPosition(clawOpener.getPosition());
+    }
 }
 
