73c25acd389def5cbce21ed2eb791e93b0ba2768
Daniel <danielgtravels@gmail.com>
2025-03-13 11:08:49 -0400

    Extended opmode instead of teleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/TeleopNothing.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/TeleopNothing.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode.teleop;
 
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.pipelines.ConfidenceOrientationVectorPipeline;
@@ -7,7 +8,7 @@ import org.firstinspires.ftc.teamcode.utilities.BottomClaw;
 import org.firstinspires.ftc.teamcode.utilities.SubsystemManager;
 
 @TeleOp(name = "TeleopNothing", group = "User Control")
-public class TeleopNothing extends Teleop{
+public class TeleopNothing extends OpMode {
 
     @Override
     public void init() {
